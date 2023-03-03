1107ccf0d42cfe4b40359e72835cfc16659718d1
manutebi <ma.aninir@duocuc.cl>
2023-03-03 17:50:45 -0300

    Ajax formato json

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 $(document).ready(function() {
-	var apiKey = 'keykY5YjFxN23izT6';
-	var baseId = 'appTsErgl1kPnkWgl';
-	var table = 'Accounts';
-
-	var url = 'https://api.airtable.com/v0/' + baseId + '/' + table + '?fields=Name, Industry, Size, Company website, Company LinkedIn, Contacts, Opportunities, HQ address, Map cache  ';
-
-	$.ajax({
-		url: url,
-		headers: { 'Authorization': 'Bearer ' + apiKey },
-		type: 'GET',
-		dataType: 'json',
-		success: function(data) {
-			// Aquí se procesa los datos recibidos
-		},
-		error: function(xhr, textStatus, errorThrown) {
-			console.log(xhr.responseText);
-		}
-	});
-});
+    // Configurar la solicitud AJAX
+    var settings = {
+      "url": "https://api.airtable.com/v0/appU7lYsFSoNH4hGO/Productos?view=Grid%20view",
+      "method": "GET",
+      "timeout": 0,
+      "headers": {
+        "Authorization": "Bearer keykY5YjFxN23izT6",
+        "Content-Type": "application/json"
+      },
+    };
+  
+    // Hacer la solicitud AJAX
+    $.ajax(settings).done(function (response) {
+      // Procesar los datos
+      var dataArray = [];
+      $.each(response.records, function(index, value) {
+        var record = value.fields;
+        record.id = value.id;
+        dataArray.push(record);
+      });
+  
+      // Inicializar DataTables
+      $('#myTable').DataTable({
+        data: dataArray,
+        columns: [
+          { title: 'Nombre', data: 'Nombre' },
+          { title: 'Descripcion', data: 'Descripcion' },
+          { title: 'Precio', data: 'Precio' }
+        ]
+      });
+    });
+  });
